e990f82e162078544ecce5908adc2af13e26e7f2
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-01-26 14:30:19 +0000

    recursion method

--- Recursion/recursion.py
+++ Recursion/recursion.py
@@ -8,3 +8,39 @@ def openRussianDoll(doll):
 
 
 openRussianDoll(4)
+
+
+# def recursionMethod(parameters):
+#     if  exit from condition satisfied:
+#         return some value
+#     else:
+#         recursionMethod(modified parameters)
+
+
+def firstMethod():
+    secondMethod()
+    print("I am the first Method")
+
+def secondMethod():
+    thirdMethod()
+    print("I am the second Method")
+
+def thirdMethod():
+    fourthMethod()
+    print("I am the third Method")
+
+def fourthMethod():
+    print("I am the fourth Method")
+
+
+firstMethod()
+
+
+def recursiveMethod(n):
+    if n<1:
+        print("n is less than 1")
+    else: 
+        recursiveMethod(n-1)
+        print(n)
+
+recursiveMethod(4)
