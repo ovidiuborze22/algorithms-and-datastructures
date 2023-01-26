11396f7093ec12495a2208c03ec9019cd555e92c
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-01-26 14:33:14 +0000

    recursion vs iteration

--- Recursion/recursion.py
+++ Recursion/recursion.py
@@ -44,3 +44,25 @@ def recursiveMethod(n):
         print(n)
 
 recursiveMethod(4)
+
+## Recursion vs Iteration###
+
+def powerOfTwo(n):
+    if n == 0:
+         return 1
+    else:
+        power = powerOfTwo(n-1)
+        return power * 2
+
+print(powerOfTwo(3))
+
+def powerOfTwoIt(n):
+    i = 0
+    power = 1
+    while i < n:
+        power = power * 2
+        i = i + 1
+    return power
+
+
+print(powerOfTwoIt(4))
