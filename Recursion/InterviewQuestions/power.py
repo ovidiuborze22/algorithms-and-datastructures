5f12c97d01ecaf17476bd52bcdc0076688057d90
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-01-26 18:11:58 +0000

    power of a number

---
+++ Recursion/InterviewQuestions/power.py
@@ -0,0 +1,11 @@
+# How to calculate power of a number using recursion
+
+def power(base,exp):
+    if exp == 0:
+        return 1
+    if(exp==1):
+        return(base)
+    if(exp!=1):
+        return (base*power(base,exp-1))
+
+print(power(4,2))
