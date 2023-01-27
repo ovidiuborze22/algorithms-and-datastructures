c60256565a0c646d0a2c465298f25f99a0661564
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-01-27 11:32:37 +0000

    linear time complexity

--- Big O Notation/timeComplexities.py
+++ Big O Notation/timeComplexities.py
@@ -5,3 +5,8 @@ array = [1, 2, 3, 4, 5]
 ######  Constant time complexity  #######
 print('######  Constant time complexity  #######')
 print(array[0])
+
+######  Linear time complexity  #######
+print('######  Linear time complexity  #######')
+for element in array:
+     print(element)
