0bc6e3be579c1ce2fdbc324082262e10b3b58ad4
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-01-27 11:39:24 +0000

    iterative algorithm

--- Big O Notation/timeComplexities.py
+++ Big O Notation/timeComplexities.py
@@ -46,3 +46,16 @@ for b in arrayB:
 for a in arrayA:
     for b in arrayB:
         print(a,b)
+
+######  Iterative algorithm - finding the biggest number in the array ####### 
+print('######  Iterative algorithm  #######')
+sample1Array = [1,10,45,33,23,45,67,2,3,33,55,11,65,76,34,35,27,99]
+
+def findBiggestNumber(sampleArray):
+    biggestNumber = sampleArray[0]
+    for index in range(1,len(sampleArray)):
+        if sampleArray[index] > biggestNumber:
+            biggestNumber = sampleArray[index]
+    print(biggestNumber)
+
+findBiggestNumber(sample1Array)
