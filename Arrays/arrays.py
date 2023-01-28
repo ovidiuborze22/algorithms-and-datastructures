6f9cc83fb416943581f60476767722d8a18778fd
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-01-28 19:02:09 +0000

    append any value to the array using append() method

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -11,3 +11,9 @@ for i in my_array:
 # 2. Access individual elements through indexes
 print("Step 2")
 print(my_array[3])
+
+# 3. Append any value to the array using append() method
+
+print("Step 3")
+my_array.append(6)
+print(my_array)
