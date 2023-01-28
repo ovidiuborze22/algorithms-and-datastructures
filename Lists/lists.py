627d1dd849b6cc361ba843cb850b8775a834333f
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-01-28 21:28:54 +0000

    accessing/traversing the list

---
+++ Lists/lists.py
@@ -0,0 +1,12 @@
+# Python Lists
+
+# Accessing/Traversing the list
+
+shoppingList = ['Milk', 'Cheese', 'Butter']
+
+for i in range(len(shoppingList)):
+    shoppingList[i] = shoppingList[i]+"+"
+    # print(shoppingList[i])
+empty = []
+for i in empty:
+    print("I am empty")
