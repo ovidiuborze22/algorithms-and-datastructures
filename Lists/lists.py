1cf1a4e2974b5629723c76d1fc0b0e675a51d8df
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-01-28 22:02:35 +0000

    slice/delete from a list

--- Lists/lists.py
+++ Lists/lists.py
@@ -1,7 +1,6 @@
 # Python Lists
 
 # Accessing/Traversing the list
-
 shoppingList = ['Milk', 'Cheese', 'Butter']
 
 for i in range(len(shoppingList)):
@@ -12,7 +11,6 @@ for i in empty:
     print("I am empty")
 
 # Update/Insert - List 
-
 myList = [1,2,3,4,5,6,7]
 print(myList)
 myList.insert(4,15)
@@ -22,3 +20,17 @@ myList.append(55)
 newList = [11,12,13,14]
 myList.extend(newList)
 print(myList)
+
+# Slice/Delete from a List
+sliceList = ['a', 'b', 'c', 'd', 'e', 'f']
+print(sliceList[0:2])
+print(sliceList[:2])
+print(sliceList[1:])
+print(sliceList[:])
+sliceList[0:2] = ['x', 'y']
+sliceList.pop()
+print(sliceList)
+del sliceList[:2]
+print(sliceList)
+sliceList.remove('e')
+print(sliceList)
