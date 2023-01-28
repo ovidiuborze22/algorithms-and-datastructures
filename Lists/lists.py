9cd9d0b383872c24bd0b8a672b3a3c215b43a38c
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-01-28 22:19:48 +0000

    list operations / functions

--- Lists/lists.py
+++ Lists/lists.py
@@ -45,3 +45,28 @@ def searchinList(list, value):
     return 'The value does not exist in the list'
 
 print(searchinList(myList, 100))
+
+#  List operations / functions
+total = 0 
+count = 0
+while (True):
+    inp = input('Enter a number: ') 
+    if inp == 'done': break
+    value = float(inp)
+    total = total + value
+    count = count + 1 
+    average = total / count
+					
+print('Average:', average)
+
+
+
+numlist = list() 
+while (True):
+    inp = input('Enter a number: ') 
+    if inp == 'done': break
+    value = float(inp)
+    numlist.append(value)
+					
+average = sum(numlist) / len(numlist) 
+print('Average:', average)
