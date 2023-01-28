18ffe6de8d36166f5311464d8416d1e728cb5eae
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-01-28 22:08:18 +0000

    searching for an element in the list

--- Lists/lists.py
+++ Lists/lists.py
@@ -34,3 +34,14 @@ del sliceList[:2]
 print(sliceList)
 sliceList.remove('e')
 print(sliceList)
+
+#  Searching for an element in the List
+myList =  [10,20,30,40,50,60,70,80,90]
+
+def searchinList(list, value):
+    for i in list:
+        if i == value:
+            return list.index(value)
+    return 'The value does not exist in the list'
+
+print(searchinList(myList, 100))
