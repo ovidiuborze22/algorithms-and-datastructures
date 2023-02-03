e0d3daa9fbf2a7ba1b3edfc6db54a4b9e440b6c0
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-02-03 12:16:42 +0000

    delete or remove a dictionary

--- Dictionaries/dictionary.py
+++ Dictionaries/dictionary.py
@@ -12,3 +12,18 @@ def traverseDict(dict):
         print(key, dict[key])
 
 traverseDict(myDict)
+
+#  Searching a dictionary
+def searchDict(dict, value):
+    for key in dict:
+        if dict[key] == value:
+            return key, value
+    return 'The value does not exist'
+print(searchDict(myDict, 27))
+
+#  Delete or remove a dictionary
+myDict.pop('address')
+print(myDict)
+#popitem()
+# clear()
+# del myDict['age']
