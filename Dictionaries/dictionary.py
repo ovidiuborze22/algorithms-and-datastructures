195a525830f799d22b996704febbcda1a610370c
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-02-03 12:06:50 +0000

    traverse through a dictionary

--- Dictionaries/dictionary.py
+++ Dictionaries/dictionary.py
@@ -6,3 +6,9 @@ myDict['age'] = 28
 myDict['address'] = 'London'
 print(myDict)
 
+#  Traverse through a dictionary
+def traverseDict(dict):
+    for key in dict:
+        print(key, dict[key])
+
+traverseDict(myDict)
