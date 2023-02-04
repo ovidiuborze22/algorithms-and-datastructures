4cf292b0ac406f2b2657837e99ab6ff7d20b1efe
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-02-04 16:39:24 +0000

    multiprocessing queue

---
+++ Queue/multiprocessingQueue.py
@@ -0,0 +1,9 @@
+# Multiprocessing Queue
+
+# How to use multiprocessing.Queue as a FIFO queue:
+
+from multiprocessing import Queue
+
+customQueue = Queue(maxsize= 3)
+customQueue.put(1)
+print(customQueue.get())
