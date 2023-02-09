ccc68b5fdb9df5fbd9adfa2433296e0008c32a11
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-02-09 18:53:37 +0000

    number factor - top down and bottom up

---
+++ Dynamic-Programming/numberFactor.py
@@ -0,0 +1,27 @@
+# Number Factor Problem using Dynamic Programming in Python
+# Top Down Approach
+
+def numberFactorTD(n, tempDict):
+    if n in (0,1,2):
+        return 1
+    elif n ==3:
+        return 2
+    else:
+        if n not in tempDict:
+            subP1 = numberFactorTD(n-1, tempDict)
+            subP2 = numberFactorTD(n-3, tempDict)
+            subP3 = numberFactorTD(n-4, tempDict)
+            tempDict[n] = subP1 + subP2 + subP3
+        return tempDict[n]
+
+print(numberFactorTD(5, {}))
+
+# Bottom Up
+
+def numberFactorBU(n):
+    tempArr = [1,1,1,2]
+    for i in range(4, n+1):
+        tempArr.append(tempArr[i-1]+tempArr[i-3]+tempArr[i-4])
+    return tempArr[n]
+
+print(numberFactorBU(5))
