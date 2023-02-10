2f15bc8242a053d23163685c264a9e63f805876e
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-02-10 14:54:06 +0000

    power

---
+++ Recursion/RecursionProblems/power.py
@@ -0,0 +1,10 @@
+# Power
+
+def power(base, exponent):
+    if exponent == 0:
+        return 1
+    return base * power(base, exponent-1)
+
+print(power(2,0)) # 1
+print(power(2,2)) # 4
+print(power(2,4)) # 16
