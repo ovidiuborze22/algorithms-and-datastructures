ab0759019fe72ab596a9b6e07cb95fefbcc43fac
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-02-10 14:56:32 +0000

    factorial

---
+++ Recursion/RecursionProblems/factorial.py
@@ -0,0 +1,11 @@
+# Factorial
+
+def factorial(num):
+    if num <= 1:
+        return 1
+    return num * factorial(num-1)
+
+print(factorial(1)) # 1
+print(factorial(2)) # 2
+print(factorial(4)) # 24
+print(factorial(7)) # 5040
