b82e847d523d615b78e45513724c3bf26a7758d0
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-02-10 15:02:57 +0000

    fibonacci

---
+++ Recursion/RecursionProblems/fib.py
@@ -0,0 +1,11 @@
+# Fibonacci
+
+def fib(num):
+    if (num < 2):
+        return num
+    return fib(num - 1) + fib(num - 2)
+
+print(fib(4)) # 3
+print(fib(10)) # 55
+print(fib(28)) # 317811
+print(fib(35)) # 9227465
