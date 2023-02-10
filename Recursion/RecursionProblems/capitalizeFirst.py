d8090915c6cc50b9eb1a67762e8d261cf20cf0c0
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-02-10 15:15:35 +0000

    capitalize first

---
+++ Recursion/RecursionProblems/capitalizeFirst.py
@@ -0,0 +1,10 @@
+# Capitalize First
+
+def capitalizeFirst(arr):
+    result = []
+    if len(arr) == 0:
+        return result
+    result.append(arr[0][0].upper() + arr[0][1:])
+    return result + capitalizeFirst(arr[1:])
+
+print(capitalizeFirst(['car', 'taco', 'banana'])) # ['Car','Taco','Banana']
